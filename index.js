171b9b2cce7d4cd5f8e4f52548e268d7f79757c3
lanyshi <lanyshi@outlook.com>
2021-03-11 19:44:48 -0500

    Add checkbox and Completed Tasks list

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ mongoose.connect('mongodb://localhost/git', {useNewUrlParser: true, useUnifiedTo
 var todoSchema = new mongoose.Schema({
     name: String
 });
+var completedSchema = new mongoose.Schema({
+    name: String
+});
 
 var Todo = mongoose.model("Todo", todoSchema);
+var Completed = mongoose.model("Completed", completedSchema);
 
 // server listening on route 3000
 app.listen(3000, function () {
@@ -22,14 +26,17 @@ app.listen(3000, function () {
 });
 
 
-
-
 // default route
 app.get('/', function (req, res) {
     Todo.find({}, function (err, todoList) {
         if (err) console.log(err);
         else {
-            res.render('index', {todoList: todoList});
+            Completed.find({}, function (err, completedList) {
+                if (err) console.log(err);
+                else {
+                    res.render('index', {todoList: todoList, completedList: completedList});
+                }
+            });
         }
     })
 });
@@ -39,7 +46,25 @@ app.post('/addtodo', function (req, res) {
     var new_todo = new Todo({
         name: req.body.newtodo
     });
-    Todo.create(new_todo, function (err, Todo) {
+    Todo.create(new_todo, function (err) {
+        if (err) console.log(err);
+        else {
+            res.redirect('/');
+        }
+    })
+});
+
+// complete task
+app.post('/completetodo', function (req, res) {
+    Todo.deleteOne({
+        name: req.body.completed
+    }, function (err) {
+        if (err) consol.log(err);
+    });
+    var new_completed = new Completed({
+        name: req.body.completed
+    });
+    Completed.create(new_completed, function (err) {
         if (err) console.log(err);
         else {
             res.redirect('/');
